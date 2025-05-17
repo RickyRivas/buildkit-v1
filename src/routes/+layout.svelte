44cd4f4dc0e767385fbc9961291b4663dbb5c9e9
Ricky Rivas <rickyrivas918@gmail.com>
2025-05-17 12:18:32 -0500

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import './styles.less';
+	import type { LayoutProps } from './$types';
+	import Navigation from '$lib/components/layout/Navigation.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
+	import { getNavRoutes } from '$lib/navigation';
+
+	const { children }: LayoutProps = $props();
+	const routes = getNavRoutes('public');
+</script>
+
+<!-- theme -->
+<svelte:head>
+	<script>
+		{
+			const theme = localStorage.getItem('sv:theme');
+
+			document.documentElement.classList.add(
+				!theme || theme === 'system'
+					? window.matchMedia('(prefers-color-scheme: dark)').matches
+						? 'dark'
+						: 'light'
+					: theme
+			);
+		}
+	</script>
+</svelte:head>
+
+<Navigation navType="public" {routes} />
+<main>
+	{@render children()}
+</main>
+<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {});
+</script>
+
+<h1>Rivas Web designs 2025 buildkit!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<section id="hero" class="hero">
+	<div class="hero__mod">
+		<h1>Small Business Web Development & Design</h1>
+		<p>
+			We are a Tulsa Web Design agency that provides custom-coded, professionally designed websites
+			that deliver premium results. No WordPress or Page-builders.
+		</p>
+	</div>
+</section>
